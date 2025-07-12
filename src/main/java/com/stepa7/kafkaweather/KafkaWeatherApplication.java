8a73f00ff556e625d31922ecff9ab048bcc41217
Stepan <sachumovitskii@edu.hse.ru>
2025-07-12 22:15:10 +0300

    Initial commit

---
+++ src/main/java/com/stepa7/kafkaweather/KafkaWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.stepa7.kafkaweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stepa7/kafkaweather/KafkaWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepa7.kafkaweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
